a4a6324197b07a0c75c97810bb764e126c615802
vikingo-44 <gonza.federico44@gmail.com>
2026-05-19 17:20:42 -0300

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-const { withExpo } = require('@expo/next-adapter');
-const nextConfig = {
-  output: 'export', 
-  images: {
-    unoptimized: true,
-  },
-};
-module.exports = withExpo(nextConfig);
